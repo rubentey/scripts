d20dd275925972f8c96708fe064cd2a9e6e712e1
rubentey <65304294+rubentey@users.noreply.github.com>
2021-03-30 23:23:48 +0200

    Update run-New_GPIO_Service.sh
    ahora crea los archivos sh, on.py y off.py correctamente, aun falta declarar la ruta destino final

--- script prueba LED GPIO/run-New_GPIO_Service.sh
+++ script prueba LED GPIO/run-New_GPIO_Service.sh
@@ -28,7 +28,7 @@ fi
 read -p "2/2 - Nombre de Servicio (p.e.: cups.service): " nomServ
 $a
 
-systemctl status $nomServ | head -3 | tail -1 | grep "(running)" > /dev/null
+systemctl status $nomServ.service | head -3 | tail -1 | grep "(running)" > /dev/null
 
 if [ $? = 0 ];
 then
@@ -37,7 +37,7 @@ else
         $a "¡El servicio '$nomServ', está parado o no funciona bien!"
 	$a "Mira su estado actual:"
 	$a
-	systemctl status $nomServ | head -10 | grep "Active:" > /dev/tty
+	systemctl status $nomServ.service | head -10 | grep "Active:" > /dev/tty
 fi
 $a
 
@@ -55,17 +55,41 @@ read -p "¿Quieres continuar? [s/n] " continuar
 
 if [ $continuar == 's' ] || [ $continuar == 'S' ];
 then
+$a
 
-main="$numGPIO-$nomServ-led.txt"
 
-# Crear script led service
+# Crear script led service, led encendido, y led apagado
 
-	$a "bien"
-	$a -e "#!/bin/bash \n \nwhile true; \ndo" > $main.txt
-	$a -e "systemctl status $nomServ | head -3 | tail -1 | grep \"(running)\" > /dev/null" >> $main.txt
-	$a -e "if [ '$?' = 0 ]; \nthen \n" >> $main.txt
-	$a -e "	python $main.txt > /dev/null \n	$a \"LED SSH on\" \n	else" >> $main.txt
+	#Script bash comprueba si activo / apagado X servicio
+main="LED-$numGPIO-$nomServ"
 
+$a "1/ Script bash, hecho"
+	$a -e "#!/bin/bash \n \nwhile true; \ndo" > $main.sh
+	$a -e "systemctl status $nomServ.service | head -3 | tail -1 | grep \"(running)\" > /dev/null \n" >> $main.sh
+	$a '	if [ $? = 0 ];' >> $main.sh
+	$a -e "	then \n		python $main-on.txt > /dev/null \n		$a \"LED $nomServ on\" \n	else" >> $main.sh
+	$a -e "		python $main-on.txt > /dev/null \n		$a \"LED $nomServ off\" \n	fi \n \ndone" >> $main.sh
+
+	chmod +x $main.sh
+
+
+servLED="LED-$numGPIO-service-$nomServ"
+	#Script python de encendido
+$a "2/ Script python encendido, hecho"
+
+	$a -e "#!/bin/python \n \nimport RPi.GPIO as GPIO \nimport time \n" > $servLED-on.py
+	$a -e "GPIO.setmode(GPIO.BOARD) \nGPIO.setup($numGPIO,GPIO.OUT) \n" >> $servLED-on.py
+	$a -e "GPIO.output($numGPIO,1) \nprint (\"LED on\") \ntime.sleep(2) \n \nGPIO.cleanup()" >> $servLED-on.py
+
+	#Script python de apagado
+$a "3/ Script python apagado, hecho"
+
+	cp $servLED-on.py $servLED-temp.py
+	sed 's/(2,1)/(2,0)/g' $servLED-temp.py > $servLED-temp2.py
+	sed 's/print ("LED on")/print ("LED off")/g' $servLED-temp2.py > $servLED-off.py
+	rm -r $servLED-temp.py $servLED-temp2.py
+
+	
 
 
 else
