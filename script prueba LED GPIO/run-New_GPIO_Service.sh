515c53828d1bcb4f81659cc24878bfa4a1fbbe69
rubentey <65304294+rubentey@users.noreply.github.com>
2021-04-03 21:04:39 +0200

    Update run-New_GPIO_Service.sh
    añadidos pequeños, pero útiles detalles

--- script prueba LED GPIO/run-New_GPIO_Service.sh
+++ script prueba LED GPIO/run-New_GPIO_Service.sh
@@ -4,28 +4,52 @@ a=echo
 
 # Bienvenida
 $a
-$a " ---------------------------------------------------------------------------------"
-$a "| Bienvenid@ al asistente de RPi para .sh + .py, y LEDs indicando servicio activo |"
-$a " ---------------------------------------------------------------------------------"
-sleep 0.3
+$a " --------------------------------------------------- "
+$a "|  Bienvenid@ al asistente de RPi para .sh + .py,   |"
+$a "|  .service y para LEDs indicando servicio activo!  |"
+$a " --------------------------------------------------- "
+sleep 0.4
 $a
 
 
-# Introducir numero de GPIO para el LED y escribir nombre servicio
-read -p "1/2 - Numero de GPIO (1-40, consulta tabla GPIO): " numGPIO
 
-if [ $numGPIO -ge 1 ] && [ $numGPIO -le 40 ];
+# Introducir número de GPIO para el LED y escribir nombre servicio
+$a "GPIOs que tienen LED, 17 en total:"
+$a "╔═════════════════════════════════════════════════════════╗"
+$a "║ 1 3 5 7  9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 ║"
+$a "║ - - - █ -  █  █  █  -  -  -  -  -  -  █  █  █  █  █  -  ║"
+$a "║ - - - - -  █  -  █  █  -  █  -  -  -  -  █  -  █  █  █  ║"
+$a "║ 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 ║"
+$a "╚═════════════════════════════════════════════════════════╝"
+$a
+$a "Actualmente se usan:"
+ls -a . | grep LED- | grep .sh
+$a
+read -p "1/2 - Número de pin GPIO (el \"█\" de la tabla): " pin
+
+
+#Número de pin
+if [ $pin -ge 1 ] && [ $pin -le 40 ];
 then
-	$a "" >> /dev/null
+	if [ $pin == 7 ] || [ $pin == 11 ] || [ $pin == 12 ] || [ $pin == 13 ] || [ $pin == 15 ] || [ $pin == 16 ] || [ $pin == 18 ] || [ $pin == 22 ] || [ $pin == 29 ] || [ $pin == 31 ] || [ $pin == 32 ] || [ $pin == 33 ] || [ $pin == 35 ] || [ $pin == 36 ] || [ $pin == 37 ] || [ $pin == 38 ] || [ $pin == 40 ];
+	then
+		$a >> /dev/null
+	else
+		$a
+		$a "¡El GPIO $pin no tiene LED, recomiendo elegir otro!"
+		$a "      ¡Puede dar error si sigues adelante!"
+		$a
+	fi
 else
 	$a
-	$a "Del 1 al 40"
+	$a "Elige del 1 al 40!"
 	sleep 1.5
 	clear
 	sh $0
 fi
 
-read -p "2/2 - Nombre de Servicio (p.e.: cups.service): " nomServ
+# Servicio a controlar
+read -p "2/2 - Nombre de Servicio (p.e.: cups): " nomServ
 $a
 
 systemctl status $nomServ.service | head -3 | tail -1 | grep "(running)" > /dev/null
@@ -35,33 +59,36 @@ then
         $a "¡El servicio '$nomServ' está activo!"
 else
         $a "¡El servicio '$nomServ', está parado o no funciona bien!"
-	$a "Mira su estado actual:"
+	$a "Mira su estado actual: "
 	$a
 	systemctl status $nomServ.service | head -10 | grep "Active:" > /dev/tty
 fi
 $a
 
 
-# Comprobar que los datos introducidos son correctos
+
+# Comprobar que los datos introducidos
 $a "-----------------------------------------"
-$a "Has elegido el Pin GPIO numero: " $numGPIO
+$a "Has elegido el Pin GPIO número: " $pin
 $a "El servicio elegido es: " $nomServ
 $a "-----------------------------------------"
 $a
 
 
-# Verificar que los datos siguen adelante
-read -p "¿Quieres continuar? [s/n] " continuar
 
-if [ $continuar == 's' ] || [ $continuar == 'S' ];
+# Verificar que quiere seguir adelante
+read -p "¿Quieres continuar? [s/n]: " continuar
+
+if [ $continuar == 'S' ] || [ $continuar == 's' ];
 then
 $a
 
 
-# Crear script led service, led encendido, y led apagado
+
+# Crear .sh led estado, .py led encendido, y .py led apagado
 
 	#Script bash comprueba si activo / apagado X servicio
-main="LED-$numGPIO-$nomServ"
+main="LED-$pin-$nomServ"
 
 $a "1/ Script bash, hecho"
 	$a -e "#!/bin/bash \n \nwhile true; \ndo" > $main.sh
@@ -73,13 +100,13 @@ $a "1/ Script bash, hecho"
 	chmod +x $main.sh
 
 
-servLED="LED-$numGPIO-service-$nomServ"
+servLED="LED-$pin-service-$nomServ"
 	#Script python de encendido
 $a "2/ Script python encendido, hecho"
 
 	$a -e "#!/bin/python \n \nimport RPi.GPIO as GPIO \nimport time \n" > $servLED-on.py
-	$a -e "GPIO.setmode(GPIO.BOARD) \nGPIO.setup($numGPIO,GPIO.OUT) \n" >> $servLED-on.py
-	$a -e "GPIO.output($numGPIO,1) \nprint (\"LED on\") \ntime.sleep(2) \n \nGPIO.cleanup()" >> $servLED-on.py
+	$a -e "GPIO.setmode(GPIO.BOARD) \nGPIO.setup($pin,GPIO.OUT) \n" >> $servLED-on.py
+	$a -e "GPIO.output($pin,1) \nprint (\"LED on\") \ntime.sleep(2) \n \nGPIO.cleanup()" >> $servLED-on.py
 
 	#Script python de apagado
 $a "3/ Script python apagado, hecho"
@@ -92,6 +119,11 @@ $a "3/ Script python apagado, hecho"
 	
 
 
+
+
+
+
+
 else
 	$a "Saliendo..."
 	exit
@@ -100,4 +132,3 @@ fi
 
 
 # Fin script
-
