f914de9dc0a35a7658349878bde647f96f2c07f0
rubentey <65304294+rubentey@users.noreply.github.com>
2021-04-03 23:32:02 +0200

    Update run-New_GPIO_Service.sh
    primera version final del programa, falta pulir algun detalle

--- script prueba LED GPIO/run-New_GPIO_Service.sh
+++ script prueba LED GPIO/run-New_GPIO_Service.sh
@@ -86,6 +86,7 @@ $a
 
 #-----------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------
+rutaShPyLEDs=/etc/GPIO_LED_Services/
 
 
 # Crear .sh led estado, .py led encendido, y .py led apagado
@@ -93,30 +94,30 @@ $a "Scripts estado, encendido, y apagado:"
 	#Script bash comprueba si activo / apagado X servicio
 main="LED-$pin-$nomServ"
 
-$a "1/3 - Script bash, hecho."
-	$a -e "#!/bin/bash \n# $main.sh \n \nwhile true; \ndo" > $main.sh
-	$a -e "systemctl status $nomServ.service | head -3 | tail -1 | grep \"(running)\" > /dev/null \n" >> $main.sh
+$a "1/3 - Script bash estado servicio, hecho."
+	$a -e "#!/bin/bash \n# $main.sh \n \nwhile true; \ndo" > $rutaShPyLEDs$main.sh
+	$a -e "systemctl status $nomServ.service | head -3 | tail -1 | grep \"(running)\" > /dev/null \n" >> $rutaShPyLEDs$main.sh
 	$a '	if [ $? = 0 ];' >> $main.sh
-	$a -e "	then \n		python $main-on.txt > /dev/null \n		$a \"LED $nomServ on\" \n	else" >> $main.sh
-	$a -e "		python $main-on.txt > /dev/null \n		$a \"LED $nomServ off\" \n	fi \n \ndone" >> $main.sh
+	$a -e "	then \n		python $main-on.txt > /dev/null \n		$a \"LED $nomServ on\" \n	else" >> $rutaShPyLEDs$main.sh
+	$a -e "		python $main-on.txt > /dev/null \n		$a \"LED $nomServ off\" \n	fi \n \ndone" >> $rutaShPyLEDs$main.sh
 
-	chmod +x $main.sh
+	chmod +x $rutaShPyLEDs$main.sh
 
 
 servLED="LED-$pin-$nomServ"
 	#Script python de encendido
 $a "2/3 - Script python encendido, hecho."
 
-	$a -e "#!/bin/python \n# $servLED.py \n \nimport RPi.GPIO as GPIO \nimport time \n" > $servLED-on.py
-	$a -e "GPIO.setmode(GPIO.BOARD) \nGPIO.setup($pin,GPIO.OUT) \n" >> $servLED-on.py
-	$a -e "GPIO.output($pin,1) \nprint (\"LED on\") \ntime.sleep(2) \n \nGPIO.cleanup()" >> $servLED-on.py
+	$a -e "#!/bin/python \n# $servLED.py \n \nimport RPi.GPIO as GPIO \nimport time \n" > $rutaShPyLEDs$servLED-on.py
+	$a -e "GPIO.setmode(GPIO.BOARD) \nGPIO.setup($pin,GPIO.OUT) \n" >> $rutaShPyLEDs$servLED-on.py
+	$a -e "GPIO.output($pin,1) \nprint (\"LED on\") \ntime.sleep(2) \n \nGPIO.cleanup()" >> $rutaShPyLEDs$servLED-on.py
 
 	#Script python de apagado
 $a "3/3 - Script python apagado, hecho."
 
-	sed 's/(2,1)/(2,0)/g' $servLED-on.py > $servLED-temp.py
-	sed 's/print ("LED on")/print ("LED off")/g' $servLED-temp.py > $servLED-off.py
-	rm -r $servLED-temp.py
+	sed 's/(2,1)/(2,0)/g' $rutaShPyLEDs$servLED-on.py > $rutaShPyLEDs$servLED-temp.py
+	sed 's/print ("LED on")/print ("LED off")/g' $rutaShPyLEDs$servLED-temp.py > $rutaShPyLEDs$servLED-off.py
+	rm -r $rutaShPyLEDs$servLED-temp.py
 
 
 #-----------------------------------------------------------------------------------------------------
@@ -128,11 +129,64 @@ $a
 $a "Servicios controlar script .sh anterior:"
 	# Servicios systemd
 servicio="LED-$pin-$nomServ-servicio.service"
+rutaSysd=/etc/systemd/system/LED-Servicios/
 
-$a "1/ - Servicio para $nomServ, hecho."
-	
+$a "1/3 - Servicio, crear ruta carpeta, hecho."
+	mkdir $rutaSysd
+
+$a "2/3 - Servicio, crear archivo .service de $nomServ, hecho."
+	$a -e "[Unit] \nDescription=Servicio de $nomServ, para LED en GPIO $pin." > $rutaSysd$servicio
+	$a -e "[Service] \nType=Simple \nExecStart=$rutaShPyLEDs$main.sh \nRestart=on-failure" >> $rutaSysd$servicio
+	$a -e "[Install] \nWantedBy=multi-user.target" >> $rutaSysd$servicio
+
+$a "3/3 - Servicio, haciendo stop y start, hecho."
+	systemctl stop $servicio
+	systemctl start $servicio
+
+#-----------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 
 
+$a
+# Resumen de lo que se ha hecho
+
+$a " ----------- "
+$a "| Servicio: |"
+$a " ----------- "
+$a
+systemctl status $servicio | head -3 | tail -1 | grep "(running)" > /dev/null
+
+if [ $? = 0 ];
+then
+        $a "¡El servicio '$nomServ' está activo!"
+else
+        $a "¡El servicio '$nomServ', está parado o no funciona bien!"
+fi
+$a
+systemctl status $nomServ.service | head -10 | grep "Active:" > /dev/tty
+$a
+
+$a " ---------------- "
+$a "| Rutas creadas: |"
+$a " ---------------- "
+$a
+$a "En /etc/ para guardar los scripts:"
+ls /etc/ | grep LED
+$a
+$a "En /etc/systemd/system para guardar los servicios:"
+$a
+ls /etc/systemd/system/ | grep LED
+
+$a
+$a " ----------------------------- "
+$a "| Servicios asignados a LEDs: |"
+$a " ----------------------------- "
+$a
+ls $rutaShPyLEDs | grep .sh
+
+$a
+$a
+$a "Fin del programa."
 
 
 else
