23db376b96b684f2d8e7d9c1ade50af7ed450e98
rubentey <65304294+rubentey@users.noreply.github.com>
2021-08-15 19:47:42 +0200

    mensaje prelogin (terminal, ssh, etc)
    mensaje prelogin (terminal, ssh, etc)

---
+++ script inicio/mensaje-prelogin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat /etc/issue.net > /etc/issue
+echo " " >> /etc/issue
+echo "PC de Rubén" >> /etc/issue
+echo " " >> /etc/issue
+
+echo "Hostname: $(hostname)" >> /etc/issue
+echo " " >> /etc/issue
+
+echo "IP: $(hostname -I)" >> /etc/issue
+echo " " >> /etc/issue
+
